b5ed370e11404314a967d656eea9a9365be94016
anarosaa <silvaanarosa103@gmail.com>
2022-06-11 13:53:41 -0300

    trazendo a lógica para o VnW

---
+++ script.js
@@ -0,0 +1,12 @@
+let name ="Ana";
+let number = 55;
+
+let year = 2022;
+let Age = year - number;{
+    console.log(2.077);
+}
+
+let day='quartaFeira';
+if('true'){
+    console.log('quartaFeira')
+}
